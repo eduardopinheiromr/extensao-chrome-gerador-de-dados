1c73f5867e73789e0c75904bef027af4952ed330
Eduardo Pinheiro <3dup1n@gmail.com>
2022-07-16 21:04:53 -0300

    fix: phone generator

--- src/utils/generatePhone.ts
+++ src/utils/generatePhone.ts
@@ -1,14 +1,19 @@
 import { ddds } from "../constants/ddds";
 
+const validSecondDigits = [9, 8, 7, 6, 5];
+
 const randomNumber = () => Math.floor(Math.random() * 10);
 
 const getRandomDDD = () => {
   return ddds[Math.floor(Math.random() * ddds.length)];
 };
 
+const secondDigit = () =>
+  validSecondDigits[Math.floor(Math.random() * validSecondDigits.length)];
+
 export const generatePhone = ({ withPontuation }: GeneratorProps): string => {
   if (withPontuation)
-    return `(${getRandomDDD()}) 9${randomNumber()}${randomNumber()}${randomNumber()}${randomNumber()}-${randomNumber()}${randomNumber()}${randomNumber()}${randomNumber()}`;
+    return `(${getRandomDDD()}) 9${secondDigit()}${randomNumber()}${randomNumber()}${randomNumber()}-${randomNumber()}${randomNumber()}${randomNumber()}${randomNumber()}`;
 
   return `${getRandomDDD()}${randomNumber()}9${randomNumber()}${randomNumber()}${randomNumber()}${randomNumber()}${randomNumber()}${randomNumber()}${randomNumber()}`;
 };
